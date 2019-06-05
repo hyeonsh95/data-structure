e29739521f6cbe1d4ee53d0e9a8727f1d7a027d9
hyeonsh95 <hyeonsh95@gmail.com>
2019-06-05 12:41:34 +0900

    Add files via upload

---
+++ 실습01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int getLeng(int *ar,int size) {
+	int leng;
+	leng = sizeof(*ar)*size;
+	return leng;
+}
+int getMax(int ar[], int size)
+{
+	int temp = ar[0];
+	for (int i = 1; i < size; i++) {
+		if (temp < ar[i])
+			temp = ar[i];
+	}
+	return temp;
+}
+int getMin(int ar[], int size)
+{
+	int temp = ar[0];
+	for (int i = 1; i < size; i++) {
+		if (temp > ar[i])
+			temp = ar[i];
+	}
+	return temp;
+}
+int main()
+{
+	int score[10] = {99,89,98,97,94,97,96,79,89};
+	int max, min, length;
+	max = getMax(score,10);
+	min = getMin(score,10);
+	length = getLeng(score,10);
+	printf("최대 : %d\n  최소: %d\n  길이: %d\n", max, min, length);
+}
