8cd6ce397cf9cdf030cfc1e3bb90f2d17dace130
hyeonsh95 <hyeonsh95@gmail.com>
2019-07-11 16:41:56 +0900

    Add files via upload

---
+++ c#/1기본컨트롤/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace _20141527E1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 623);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 98);
+            this.textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(50, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 145);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[이 름]";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 65);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(50, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 129);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "[성 별]";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(267, 34);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "남자";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(267, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "여자";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "남자",
+            "여자"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(50, 346);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(442, 211);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "[취 미]";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(267, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "독서";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(267, 85);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "운동";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(267, 130);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 16);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "낚시";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(55, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(121, 148);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 570);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(442, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "출 력";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 728);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ c#/1기본컨트롤/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20141527E1
+{
+    public partial class Form1 : Form
+    {
+        private bool[] m_Checked = new bool[3] { false, false, false };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //comboBox1.Items.Add("남자");
+            //comboBox1.Items.Add("여자");
+            listBox1.Items.Add("독서");
+            listBox1.Items.Add("낚시");
+            listBox1.Items.Add("운동");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hobby = " ";
+
+            if (checkBox1.Checked == true)
+            {
+                hobby += checkBox1.Text.ToString() + " ";
+            }
+            if (checkBox2.Checked == true)
+            {
+                hobby += checkBox2.Text.ToString() + " ";
+            }
+            if (checkBox3.Checked == true)
+            {
+                hobby += checkBox3.Text.ToString();
+            }
+
+            if (textBox2.Text == "")//이름x
+            {
+                MessageBox.Show("이름을 입력하십시오");
+            }
+            else if (textBox2.Text != "")//이름o
+            {
+                if (radioButton1.Checked == true || radioButton2.Checked == true)
+                {
+                    if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox3.Checked == true)
+                    {
+                        textBox1.Text = "이름 : " + textBox2.Text + "\r\n" + "성별 : " + radioButton1.Text.ToString() + "\r\n" + "취미 :" + hobby;
+                    }
+                    else if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false)
+                    {
+                        textBox1.Text = "이름 : " + textBox2.Text + "\r\n" + "성별 : " + radioButton1.Text.ToString();
+                    }
+                }
+                else if (radioButton1.Checked == false && radioButton2.Checked == false)
+                {
+                    if (checkBox1.Checked == true || checkBox2.Checked == true || checkBox3.Checked == true)
+                    {
+                        textBox1.Text = "이름 : " + textBox2.Text + "\r\n" + "취미 :" + hobby+ "\n";
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                radioButton1.Checked = true;
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = radioButton1.Text.ToString();
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = radioButton2.Text.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (int index in listBox1.SelectedIndices)
+            {
+                if (index == 0)
+                {
+                    m_Checked[0] = true;
+                    checkBox1.Checked = true;
+                }
+
+                else if (index == 1)
+                {
+                    m_Checked[1] = true;
+                    checkBox2.Checked = true;
+                }
+                else if (index == 2)
+                {
+                    m_Checked[2] = true;
+                    checkBox3.Checked = true;
+                }
+            }
+
+            if (listBox1.GetSelected(0) == false)
+            {
+                checkBox1.Checked = false;
+            }
+            if (listBox1.GetSelected(1) == false)
+            {
+                checkBox2.Checked = false;
+            }
+            if (listBox1.GetSelected(2) == false)
+            {
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if(m_Checked[0] == true)
+            {
+                m_Checked[0] = false;
+                listBox1.SetSelected(0, false);
+            }
+            else
+            {
+                m_Checked[0] = true;
+                listBox1.SetSelected(0, true);
+            }
+        }
+
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+            if (m_Checked[1] == true)
+            {
+                m_Checked[1] = false;
+                listBox1.SetSelected(1, false);
+            }
+            else
+            {
+                m_Checked[1] = true;
+                listBox1.SetSelected(1, true);
+            }
+        }
+
+        private void checkBox3_Click(object sender, EventArgs e)
+        {
+            if (m_Checked[2] == true)
+            {
+                m_Checked[2] = false;
+                listBox1.SetSelected(2, false);
+            }
+            else
+            {
+                m_Checked[2] = true;
+                listBox1.SetSelected(2, true);
+            }
+        }
+    }
+}
